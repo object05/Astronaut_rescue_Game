7b6618af0b5adefbf251e0225438a7adad93a8d1
object05 <alan.hablak@gmail.com>
2020-07-30 17:45:26 +0200

    Added sound effects

--- Assets/Scripts/Components/CollisionTriggerComponent.cs
+++ Assets/Scripts/Components/CollisionTriggerComponent.cs
@@ -1,27 +1,37 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionTriggerComponent : MonoBehaviour
 {
+    public AudioClip Hit;
+    public AudioClip Pick;
+    private AudioSource source;
+
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit detected between " + gameObject.name + " and " + other.name);
         other.GetComponent<RemoveComponent>().forceRemove();//pushes to pool
-
+        
 
         if(other.tag == "Meteor")
         {
             GameManager.instance.Damage();
-
+            source.PlayOneShot(Hit);
         }
         else
         {
             GameManager.instance.Score();
+            source.PlayOneShot(Pick);
         }
 
         //todo sound play
         //todo particle effect
-        //todo points
     }
 }
